13aec4d2e0ee0c22bc96f80e32a8ef963c249e28
swarna <starwars121012@gmail.com>
2020-07-02 00:35:00 +0530

    removing email credentials

--- AsyncTask/settings.py
+++ AsyncTask/settings.py
@@ -126,11 +126,6 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT= 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'tangodjango01@gmail.com'
-# EMAIL_HOST_PASSWORD = 'hcmpjnjhvaxqzjkp'
+
 
 
